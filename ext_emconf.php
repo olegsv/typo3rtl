1efb4a27b6a06685578bb6eafd0471f26656664e
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2019-04-08 16:46:15 +0300

    [TASK] Enable TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,10 +19,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => true,
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
+			'typo3' => '7.6.0-8.7.999',
 		),
 		'conflicts' => array(
 		),
